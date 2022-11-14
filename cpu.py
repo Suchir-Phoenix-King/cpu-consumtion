60084f9295893e2f070a66afd0c7007f6fe6feec
Suchir-Phoenix-King <88494177+Suchir-Phoenix-King@users.noreply.github.com>
2022-11-14 17:04:40 +0530

    Add files via upload

---
+++ cpu.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 12 14:25:55 2022
+
+@author: PC_RC
+"""
+
+import matplotlib .pyplot as plt
+import psutil as p
+app_name_dict = []
+count = 0
+
+for process in p.process_iter():
+    count = count+1
+    if count <= 6 :
+        name = process.name()
+        cpu_usage = p.cpu_percent()
+        print("name = ",name, "--- cpu_usage ---", cpu_usage)
+        app_name_dict.update(name, cpu_usage)
+        keymax = max(app_name_dict, key = app_name_dict.get())
+        print(keymax)
+        keymin = min(app_name_dict, key = app_name_dict.get())
+        print(keymin)
+        name_list = [keymax, keymin]
+        print(name_list)
+        app = app_name_dict.values()
+        max_app = max(app)
+        print(max_app)
+        min_app = min(app)
+        print(min_app)
+        max_min_list = [max_app, min_app]
+        
+plt.figure(figsize = (15,7))
+plt.xlabel("Application")
+plt.ylabel("Usage")
+plt.bar(name_list, max_min_list, width = 0.6, color =("blue", "green"))
+plt.show()
+        
+        
+
